cd239055255abf352937ae0c415d27c01a6b8ec2
Your Name <you@example.com>
2024-01-05 03:01:16 +0600

    Fixed navbar issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           <div className=" justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
 
             <div>
-                <div className="flex items-center justify-between py-3 md:py-4 md:block">
+                <div className="flex items-center justify-between py-3 mx-auto px-2 md:py-4 md:block">
                    {/* Logo */}
                    <Link href='/'>
                     <h2 className="text-2xl text-black font-bold">
